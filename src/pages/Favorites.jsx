52f29eae9ea093943cecbc95f5d941b0a0dc19e3
Gael Hofman <Gieltie@outlook.fr>
2023-12-29 00:45:21 +0100

    mise en place des routes et pages

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Favorites = () => {
+  return (
+    <div>
+      <h1>
+
+      Super Favorites page for movies
+      </h1>
+    </div>
+  )
+}
+
+export default Favorites
---
+++ src/pages/LatestReleases.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LatestReleases = () => {
+  return (
+    <div>
+      Ici en principe on devrait avoir les derniers films sortis
+    </div>
+  )
+}
+
+export default LatestReleases
