2a700ac880d0ce3d32f641ab72fa0ca46f3a81ee
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2023-12-29 12:39:08 +0100

    modification page Home

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { FaReact } from 'react-icons/fa'; 
-import { Link } from 'react-router-dom'; 
+import { NavLink } from 'react-router-dom'; 
 import './Navbar.scss';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="navbar__logo-title"> 
+      <NavLink exact to="/" className="navbar__logo-title"> 
         <FaReact size={50} />  {/*  Logo provisoire */}
         <h1 className='navbar__title'>MOVIE FINDER</h1>
-      </Link>
+      </NavLink>
       <div className="navbar__buttons">
-        <Link to="/">HOME</Link> 
-        <Link to="/latestreleases">LATEST RELEASES</Link> 
-        <Link to="/favorites">FAVORITES</Link> 
-        <Link to="/signup">SIGNUP</Link> 
-        <Link to="/login">LOGIN</Link> 
+        <NavLink exact to="/">HOME</NavLink> 
+        <NavLink to="/latestreleases">LATEST RELEASES</NavLink> 
+        <NavLink to="/favorites">FAVORITES</NavLink> 
+        <NavLink to="/signup">SIGNUP</NavLink> 
+        <NavLink to="/login">LOGIN</NavLink> 
       </div>
     </nav>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from 'react';
+import './Home.scss'; 
 
 const Home = () => {
   return (
-    <div>
-      <h1>
-        Super Home page
-      </h1>
+    <div className="home">
+      <h1 className="home__title">Movie Finder</h1>
+      <input className="home__search" type="text" placeholder="Search for a movie..." />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
